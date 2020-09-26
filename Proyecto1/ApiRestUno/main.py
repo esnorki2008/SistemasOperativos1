54048f59fb24c8aa3790b3c42f6914f02313cf46
AndhySolis <49577095+AndhySolis@users.noreply.github.com>
2020-09-26 12:17:53 -0600

    Configuracion Servidor

--- Proyecto1/ApiRestUno/main.py
+++ Proyecto1/ApiRestUno/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api,Resource,reqparse,abort
+from flask_restful import Api, Resource, reqparse, abort
 from server import Server
 
 
@@ -7,9 +7,19 @@ app = Flask(__name__)
 api = Api(app)
 
 
+def leer_ip():
+    try:
+        archivo = open("/home/ip.txt", "r")
+        cadena = archivo.read()
+        cadena = cadena.split("\n")
+        return cadena
+    except:
+        print("Error Leyendo Archivo Con Ips")
+        return 0,0
 
+lst_ip=leer_ip()
 
-api.add_resource(Server,"/server")
+api.add_resource(Server, "/server")
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int("5050"))
+    app.run(debug=True, host='0.0.0.0', port=int("5000"))
--- Proyecto1/ApiRestUno/server.py
+++ Proyecto1/ApiRestUno/server.py
@@ -3,10 +3,11 @@ from pymongo import MongoClient
 import requests 
 class Server(Resource):
     def __init__(self):
-        self.ip=None
+        from main import lst_ip
+        self.ip=lst_ip
     
     def get(self):
-        pass
+        return "oki",200    
 
     def put(self):
         mensaje =request.args.get('ip', None)
@@ -15,27 +16,91 @@ class Server(Resource):
                 
     def post(self):
         try:
-            print(request.args)
             mensaje =request.args.get('mensaje', None)
             userre =request.args.get('usuario', None)
             if mensaje is None:
                 return "No Se Detecto El Mensaje Enviado",409
             if userre is None:
                 return "No Se Detecto El Usuario A Enviar",409
-            print(userre,mensaje)
-            jison = {"usuario":userre,"mensaje":mensaje}
-            estado=self.Enviar(jison)
+            jison = {"usuario":userre,"mensaje":mensaje}   
+            
+            index = self.DecidirPcMensajes()
+            print(index)
+            if index == -1:
+                index = self.DecidirPcRecursos()
+            if index == -1:
+                return "No Se Pudo Decidir La Computadora Para El Mensaje",409
+            print("EnviandoServidor "+str(index))
+            estado=self.Enviar(jison,self.ip[index])
             return estado,200
         except:
             return "No Se Pudo Enviar Al Servido AoB",409
 
-    def Enviar(self,jison):
+    def DecidirPcMensajes(self):
+        Pc0=self.Contar(self.ip[0])
+        Pc1=self.Contar(self.ip[1])
+        
+        if Pc0 is None and Pc1 is not None:
+            return 1
+        elif Pc1 is None and Pc0 is not None:
+            return 0
+        elif Pc1 is None and Pc0 is None:
+            return -1
+        
+        if Pc0 > Pc1:
+            return 1
+        elif Pc1 > Pc0:
+            return 0
+        else:
+            return -1
+
+    def DecidirPcRecursos(self):
+        Pc0=self.Uso(self.ip[0])
+        Pc1=self.Uso(self.ip[1])
+        if Pc0 is None and Pc1 is not None:
+            return 1
+        elif Pc1 is None and Pc0 is not None:
+            return 0
+        elif Pc1 is None and Pc0 is None:
+            return -1
+
+        if Pc0["ram"] > Pc1["ram"]:
+            return 1
+        elif Pc1["ram"] > Pc0["ram"]:
+            return 0
+
+        if (Pc0["cpu"] > Pc1["cpu"]):
+            return 1
+        else:
+            return 0
+
+    def Enviar(self,jison,ip_actual):
         try:
             puerto = ":5000"
-            self.ip = '192.168.1.10'
-            response = requests.post("http://"+self.ip+puerto+"/mongo",
+            response = requests.post("http://"+ip_actual+puerto+"/mongo",
             params=jison)
             return response.json()
         except :
             print("Error De Conexion Con El Servidor AoB")
             return "Error De Conexion Con El Servidor AoB"
+
+    def Contar(self,ip_actual):
+        try:
+            contador = 0
+            puerto = ":5000"
+            response = requests.get("http://"+ip_actual+puerto+"/mongo")
+            for cada in response.json():
+                contador=contador+1
+            return contador
+        except :
+            print("Error De Conexion Con El Servidor AoB Consultando Tamanio")
+            return None
+    
+    def Uso(self,ip_actual):
+        try:
+            puerto = ":5000"
+            response = requests.get("http://"+ip_actual+puerto+"/mod")
+            return(response.json())
+        except :
+            print("Error De Conexion Con El Servidor AoB Consultando CPU/RAM")
+            return None
